c09281a092b5e6301f72f37742f18372256cdf0a
Wong Liang Xing <liangxingwong@ieee.org>
2022-07-02 16:34:16 +0800

    init

---
+++ FuncWithSelector.sol
@@ -0,0 +1,20 @@
+pragma solidity 0.7.0;
+
+contract FuncWithSelector {
+    
+    // When written in solidity, this will have the desired effect.
+    function testProxy() public pure returns (bytes4 selector, bytes32 selectorWord) {
+        selector = 0x12345678;
+        selectorWord = selector;
+    }
+    
+    function testMulticall() public pure returns (bytes4 selector, bytes32 selectorWord) {
+        selector = 0x12345679;
+        selectorWord = selector;
+    }
+
+    function testMulticall1() public pure returns (bytes4 selector, bytes32 selectorWord) {
+        selector = 0x12345680;
+        selectorWord = selector;
+    }
+}
---
+++ Proxy.sol
@@ -0,0 +1,45 @@
+pragma solidity ^0.7.0;
+
+contract Proxy {
+    
+    address public immutable admin;
+    
+    mapping (bytes32 => bytes32) public codeHashBySalt;
+    
+    constructor() {
+
+    }
+    
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "Only admin");
+        _;
+    }
+
+    // Receive ETH
+    receive() external payable {}
+    
+    // Delegates the call based on selector and salt.
+    fallback() external payable {
+        // Get selector / contract salt.
+
+        // Compute address of registered function.
+        // See https://solidity.readthedocs.io/en/v0.7.4/control-structures.html#salted-contract-creations-create2
+
+        // Execute call. Revert on failure or return on success.
+
+    }
+
+
+
+    // Registers a new function selector and its corresponding code.
+    function register(bytes4 selector, bytes memory code) public onlyAdmin returns (address addr, bytes32 salt) {
+
+    }
+    
+    // Retrieves the selector from calldata and the corresponding salt.
+    function _getSaltAndSelector() internal pure returns (bytes32 salt, bytes4 selector) {
+
+    }
+
+
+}
