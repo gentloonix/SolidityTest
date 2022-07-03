77189ae85108b4a51a864a064eef102780af61e3
Wong Liang Xing <liangxingwong@ieee.org>
2022-07-03 09:55:04 +0800

    truffle

--- truffle-config.js
+++ truffle-config.js
@@ -18,10 +18,10 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
   /**
@@ -74,6 +74,11 @@ module.exports = {
     //   network_id: 2111,   // This network is yours, in the cloud.
     //   production: true    // Treats this network as if it was a public net. (default: false)
     // }
+    fuji_testnet: {
+      provider: () => new HDWalletProvider(mnemonic, `https://api.avax-test.network/ext/bc/C/rpc`),
+      network_id: 43113,
+      networkCheckTimeout: 100000,
+    }
   },
 
   // Set default mocha options here, use special reporters, etc.
@@ -85,14 +90,14 @@ module.exports = {
   compilers: {
     solc: {
       version: "0.8.15",      // Fetch exact version from solc-bin (default: truffle's version)
-      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
-      //  evmVersion: "byzantium"
-      // }
+      //docker: true,         // Use "0.5.1" you've installed locally with docker (default: false)
+      settings: {             // See the solidity docs for advice about optimization and evmVersion
+        optimizer: {
+          enabled: true,
+          runs: 200
+        },
+        //evmVersion: "byzantium"
+      }
     }
   },
 
